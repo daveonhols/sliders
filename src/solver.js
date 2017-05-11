d11bd2eca30ba17042d4278cd47c657dc93c5390
David Smith <daveonhols2003@yahoo.co.uk>
2017-05-11 20:33:33 +0100

    generate and solve puzzles

--- src/solver.js
+++ src/solver.js
@@ -1,22 +1,93 @@
+const hashes = require("hashes");
+const winston = require("winston");
+
+var logger = new (winston.Logger)({
+    transports: [new (winston.transports.Console)({"timestamp":true})]
+});
+
+function hashCode(str) {
+  let hash = 0;
+  let i = 0;
+  let chr = 0;
+  let len = 0;
+  if (str.length === 0) return hash;
+  for (i = 0, len = str.length; i < len; i += 1) {
+    chr = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
 function solved(positions) {
   const last = undefined === (positions[positions.length - 1]);
   return positions.slice(0, -1).reduce((state, curr, idx) => curr === idx ? state : false, last);
 }
 
+function closestPuzzleSort(a, b) {
+  return (a.moves + a.distance) - (b.moves + b.distance);
+}
+
+function getCheckPositions(size) {
+  if (size === 3) {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  }
+  if (size === 4) {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  }
+  return [];
+}
+
+function chk_positions(a, b, size) {
+  const chk_positions = getCheckPositions(size);
+  return chk_positions.every(idx => a.positions[idx] === b.positions[idx]);
+}
+
+function visited(set, item) {
+  return set.some(curr => chk_positions(curr.current, item, item.size));
+}
+
 function solve(puzzle) {
 
   let open = [];
-  const closed = [];
+  const closed = new hashes.HashSet();
 
   open.push({ current: puzzle, moves: 0, distance: estimate(puzzle) });
 
+  let next = open.shift();
+  closed.add(hashCode(JSON.stringify(next.current)));
 
+  let best = 99999;
+  let round = 0;
+  while (next.distance > 0) {
+
+    let expanded = permutations(next.current);
+
+    if (next.distance < best) {
+      best = next.distance;
+    }
+
+    expanded = expanded.filter(item => !closed.contains(hashCode(JSON.stringify(item))));
+
+    expanded.forEach(item => {
+        open.push({ current: item, moves: next.moves + 1, distance: estimate(item) });
+        closed.add(hashCode(JSON.stringify(item)));
+      });
+
+    open.sort(closestPuzzleSort);
+
+    next = open.shift();
+
+    closed.add(hashCode(JSON.stringify(next.current)));
+    round += 1;
+  }
+
+  return next;
 
 }
 
 function estimate(puzzle) {
-  return
-    puzzle.positions.reduce((state, curr, idx) =>
+  return puzzle.positions.reduce((state, curr, idx) =>
       curr === undefined
       ? state + manhattan_dist(puzzle.size, idx, puzzle.positions.length - 1)
       : state + manhattan_dist(puzzle.size, idx, curr), 0);
@@ -34,6 +105,7 @@ function manhattan_dist(size, from, to) {
 }
 
 function permutations(puzzle) {
+
   const space = puzzle.positions.indexOf(undefined);
   const space_row = Math.floor(space / puzzle.size);
   const space_col = space % puzzle.size;
@@ -69,5 +141,5 @@ function swap(positions, from, to) {
 exports.solved = solved;
 exports.manhattan_dist = manhattan_dist;
 exports.permutations = permutations;
-
-
+exports.estimate = estimate;
+exports.solve = solve;
--- test/test.solver.js
+++ test/test.solver.js
@@ -102,3 +102,41 @@ it("permutations UL", function() {
   expect(permutations.some(item => deepequal(item, expected_permutations[1]))).to.equal(true);
 
 });
+
+it("estimate", function() {
+  let puzzle = { positions: [0, 1, 2, 3, 4, 5, 6, 7, undefined], size: 3 };
+  expect(solver.estimate(puzzle)).to.equal(0);
+  puzzle = { positions: [0, 1, 2, 3, 4, 5, 6, undefined, 7], size: 3 };
+  expect(solver.estimate(puzzle)).to.equal(2);
+  puzzle = { positions: [1, 0, 2, 3, 4, 5, 6, undefined, 7], size: 3 };
+  expect(solver.estimate(puzzle)).to.equal(4);
+  puzzle = { positions: [4, 1, 2, 3, 0, 5, 6, undefined, 7], size: 3 };
+  expect(solver.estimate(puzzle)).to.equal(6);
+  puzzle = { positions: [4, 6, 2, 3, 0, 5, 1, undefined, 7], size: 3 };
+  expect(solver.estimate(puzzle)).to.equal(12);
+});
+
+it("solve", function() {
+
+  let puzzle = { positions: [0, 1, 2, 3, 4, undefined, 6, 7, 5], size: 3 };
+  let solved = solver.solve(puzzle);
+
+  expect(solved.moves).to.equal(1);
+  expect(solver.solved(solved.current.positions)).to.equal(true);
+
+  puzzle = { positions: [0, 1, 2, 3, 4, 5, 6, 7, undefined], size: 3 }
+  solved = solver.solve(puzzle);
+
+  expect(solved.moves).to.equal(0);
+  expect(solver.solved(solved.current.positions)).to.equal(true);
+
+  puzzle = { positions: [5, undefined, 3, 2, 4, 0, 1, 6, 7], size: 3 };
+  solved = solver.solve(puzzle);
+  expect(solved.moves).to.equal(19);
+  expect(solver.solved(solved.current.positions)).to.equal(true);
+
+  puzzle = { positions: [4, 0, 6, 2, 8, 1, 10, 3, 12, 5, 14, 7, undefined, 9, 13, 11], size: 4 };
+  solved = solver.solve(puzzle);
+  expect(solver.solved(solved.current.positions)).to.equal(true);
+
+});
